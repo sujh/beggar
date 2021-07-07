fcc443db9ed9ca8800babcb4cc60ba2762516b5d
spike76 <spike76@qq.com>
2021-07-07 15:37:13 +0800

    beg service and worker

--- app/models/beggar.rb
+++ app/models/beggar.rb
@@ -1,3 +1,7 @@
 class Beggar < ApplicationRecord
   enum status: [:init, :ok, :forbidden]
+
+  def paged_site(page)
+    site.include?("${page}") ? site.sub("${page}", page.to_s) : site
+  end
 end
--- app/workers/beg_worker.rb
+++ app/workers/beg_worker.rb
@@ -2,6 +2,12 @@ class BegWorker
   include Sidekiq::Worker
 
   def perform(*args)
-    BeggarManager.new.run_forever
+    old_proxies = Hash.new(Set.new)
+    loop do
+      Beggar.where(status: :ok).each do |beggar|
+        old_proxies[beggar.name] = BegService.new(beggar).run(old_proxies[beggar.name])
+      end
+      sleep 10
+    end
   end
 end
